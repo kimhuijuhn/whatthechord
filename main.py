d229f9bb8e29484fad2260dfe9c3bee7d9a07796
huijuhnkim <kim.huijuhn@gmail.com>
2026-05-19 14:54:14 +0900

    refactor: changed main entry point to main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from wip!")
+from rtmidi import midiutil
+import sandbox
 
+def main():
+    midiin, portname = midiutil.open_midiport(0)
+    print("Current Port: ", portname)
+    sandbox.get_current_on_notes(midiin)
 
 if __name__ == "__main__":
     main()
--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,30 @@
 import time
-from rtmidi import midiutil
-import utility_functions as uf
+from Note import Note
 
+def get_current_on_notes(midiin):
+    """ Print list of currently ON notes based on message """
+    current_notes = []
 
-midiin, portname = midiutil.open_midiport(0)
-print("Current Port: ", portname)
-uf.get_current_on_notes(midiin)
+    try:
+        while True:
+            msg = midiin.get_message()
+            if msg:
+                status = msg[0][0]
+                note = Note(msg[0][1])
+
+                # store a new ON note
+                if status == 144 and note not in current_notes:
+                    current_notes.append(note)
+
+                # discard OFF note
+                if status == 128 and note in current_notes:
+                    current_notes.remove(note)
+
+                # print current ON notes
+                print(current_notes)
+
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        midiin.close_port()
