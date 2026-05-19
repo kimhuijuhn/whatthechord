acbb50d5f26b67973859f2ae7bb290289c955882
huijuhnkim <kim.huijuhn@gmail.com>
2026-05-19 15:41:48 +0900

    doc: docstring on Note.py

--- Note.py
+++ Note.py
@@ -3,16 +3,23 @@ NOTE_MAP = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 class Note:
     def __init__(self, val):
         """ Create a Note instance given a MIDI value. """
+        self._value = val
         self.pitch = NOTE_MAP[val % 12]
         self.octave = (val // 12) - 1   
 
     def __str__(self):
+        """ String representation of a Note instance (ex. C4) """
         return f"{self.pitch}{self.octave}"
     
     def __repr__(self):
+        """ String representation of a Note instance (ex. C4) """
         return f"{self.pitch}{self.octave}"
     
     def __eq__(self, other):
+        """ Two notes are equal when their octave and pitch is equal """
         if not isinstance(other, Note):
             return NotImplemented
         return self.pitch == other.pitch and self.octave == other.octave
+
+    def get_interval(self, other):
+        return self._value - other._value
