eb698a5394aab4cae0ed65d7cf8cb7fe73ad5b0a
huijuhnkim <kim.huijuhn@gmail.com>
2026-05-19 16:32:08 +0900

    refactor: renamed midi_listener.py as controller.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from rtmidi import midiutil
-import midi_listener
+import controller
 
 def main():
     midiin, portname = midiutil.open_midiport(0)
     print("Current Port: ", portname)
-    midi_listener.print_current_on_notes(midiin)
+    controller.print_current_on_notes(midiin)
 
 if __name__ == "__main__":
     main()
